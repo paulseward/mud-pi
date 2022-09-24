1402b1e4d1b6760514f98a8e819b327f82a3d992
Paul Seward-Prior <paul@paulseward.com>
2022-09-24 09:16:58 +0000

    Load map from a file

--- simplemud.py
+++ simplemud.py
@@ -22,22 +22,15 @@ author: Mark Frimston - mfrimston@gmail.com
 """
 
 import time
+import json
 
 # import the MUD server class
 from mudserver import MudServer
 
-
-# structure defining the rooms in the game. Try adding more rooms to the game!
-rooms = {
-    "Tavern": {
-        "description": "You're in a cozy tavern warmed by an open fire.",
-        "exits": {"outside": "Outside"},
-    },
-    "Outside": {
-        "description": "You're standing outside a tavern. It's raining.",
-        "exits": {"inside": "Tavern"},
-    }
-}
+# Load the map file
+f = open('map.json')
+rooms = json.load(f)
+f.close()
 
 # stores the players in the game
 players = {}
